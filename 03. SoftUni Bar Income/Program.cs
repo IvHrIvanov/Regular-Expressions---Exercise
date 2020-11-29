2029f17fc54b77e1a90707fc973a3ee26ffbd2f2
IvanHrIvanov <vinko96@abv.bg>
2020-11-29 23:52:32 +0200

    Add

--- 03. SoftUni Bar Income/Program.cs
+++ 03. SoftUni Bar Income/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace _03._SoftUni_Bar_Income
 {
@@ -6,7 +9,65 @@ namespace _03._SoftUni_Bar_Income
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string pattern = @"%(?<Name>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*[A-z]*\|(?<count>\d+)\|[^|$%.]*?(?<price>\d+\.?\d+)\$";
+
+            string input = Console.ReadLine();
+            Regex regex = new Regex(pattern);
+            double totalPrice = 0;
+
+            List<Soft> bar = new List<Soft>();
+
+            while (input != "end of shift")
+            {
+                
+                Match currentShift = regex.Match(input);
+                if (currentShift.Groups[1].Captures.Count > 0 &&
+                    currentShift.Groups[2].Captures.Count > 0 &&
+                    currentShift.Groups[3].Captures.Count > 0 &&
+                    currentShift.Groups[4].Captures.Count > 0)
+                {
+
+                    string name = currentShift.Groups[1].Value;
+                    int count = int.Parse(currentShift.Groups[3].Value);
+                    string product = currentShift.Groups[2].Value;
+                    double price = double.Parse(currentShift.Groups[4].Value) * count;
+
+                    Soft newPerson = new Soft(name, product, price);
+                    bar.Add(newPerson);
+                    totalPrice += price;
+                }
+                input = Console.ReadLine();
+            }
+            if (bar.Count != 0)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, bar));
+                Console.WriteLine($"Total income: {totalPrice:f2}");
+            }
+
         }
+    }
+
+    class Soft
+    {
+        public Soft(string person, string product, double price)
+        {
+            Person = person;
+            Product = product;
+            Price = price;
+        }
+
+        public string Person { get; set; }
+        public string Product { get; set; }
+
+        public double Price { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{Person}: {Product} - {Price:f2}");
+            return sb.ToString();
+        }
+
+
     }
 }
