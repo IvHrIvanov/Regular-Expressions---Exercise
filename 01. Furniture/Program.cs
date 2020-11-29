a9c98904280475106e05c25150a561e3c28146da
IvanHrIvanov <vinko96@abv.bg>
2020-11-29 21:20:26 +0200

    Add project files.

---
+++ 01. Furniture/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string pattern = @">>(?<product>[A-z]+)<<(?<Price>\d+\.*\d+)!(?<count>\d*)";
+
+            string input = Console.ReadLine();
+            Regex regex = new Regex(pattern);
+            double sum = 0;
+            Console.WriteLine("Bought furniture:");
+
+            while (input != "Purchase")
+            {
+
+                Match stocks = regex.Match(input);
+                if (stocks.Success)
+                {
+                    Console.WriteLine(stocks.Groups[1].Value);
+                    double currentPrice = double.Parse(stocks.Groups[2].Value);
+                    int currentCount = int.Parse(stocks.Groups[3].Value);
+                    sum += currentPrice * currentCount;
+                 
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total money spend: {sum:f2}");
+        }
+    }
+}
---
+++ 02. Race/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02._Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string patternName = @"[\W\d]";
+            string patternDigit = @"[\WA-Za-z]";
+
+            string[] names = Console.ReadLine().Split(", ");
+
+            string racer = Console.ReadLine();
+            Dictionary<string, int> allRacers = new Dictionary<string, int>();
+
+            foreach (string currentName in names)
+            {
+                allRacers.Add(currentName, 0);
+            }
+
+            while (racer != "end of race")
+            {
+
+                string name = Regex.Replace(racer, patternName, "");
+                string distance = Regex.Replace(racer, patternDigit, "");
+
+                int sum = 0;
+
+                foreach (var item in distance)
+                {
+                    sum += int.Parse(item.ToString());
+                }
+
+                if (allRacers.ContainsKey(name))
+                {
+                    allRacers[name] += sum;
+                }
+
+                racer = Console.ReadLine();
+            }
+
+            int place = 1;
+
+            foreach (var currentRacer in allRacers.OrderByDescending(x => x.Value))
+            {
+                string text = place == 1 ? "st" : place == 2 ? "nd" : "rd";
+
+                Console.WriteLine($"{place++}{text} place: {currentRacer.Key}");
+
+                if (place == 4)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
